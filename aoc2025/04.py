e8afbd87df0c4b167c5bf3e590f07aee3c78688d
Oskar Haarklou Veileborg <ohv1020@hotmail.com>
2025-12-04 08:25:07 +0100

    Day 4

---
+++ aoc2025/04.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env pypy3
+from __future__ import annotations
+
+from util import *
+
+replace_stdin()
+
+res = 0
+
+G = Grid(lines())
+
+r = G.crev
+Q = []
+for p in r["@"]:
+    cnt = 0
+    for d in OCTDIR:
+        np = p + d
+        cnt += G.at(np, default=None) == "@"
+    if cnt < 4:
+        Q.append(p)
+
+for p in Q:
+    G.set(p, ".")
+    for nd in OCTDIR:
+        np1 = p + nd
+        if G.at(np1, ".") != "@":
+            continue
+        cnt = 0
+        for d in OCTDIR:
+            np = np1 + d
+            cnt += G.at(np, default=None) == "@"
+        if cnt < 4:
+            Q.append(np1)
+
+
+res = len(set(Q))
+print("\n".join(map(str, G)))
+
+submit(res)
